7ef3bab8677ef73aac6ebaf0bd8487189a424417
adrilc24 <adrilc24@gmail.com>
2018-10-11 11:50:39 +0200

    Añadidos EjemploStream1 y 2 junto a HolaMundo1

---
+++ EjemploStream2/src/EjemploStream2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class EjemploStream2 {
+	public static void main(String args[]) throws IOException {
+		ProcessBuilder pb = new ProcessBuilder ("ls -z");
+		//pb.directory (new File("/home/adrcre/PSP/HolaMundo1/bin/"));
+		Process process = new ProcessBuilder("ls -z").start() ;
+		InputStream is = process.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String line;
+		System.out.println("Salida del proceso " + Arrays.toString(args) + " :");
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+		}
+	}
+}
---
+++ HolaMundo1/src/HolaMundo1.java
@@ -0,0 +1,8 @@
+
+public class HolaMundo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+	}
+
+}
