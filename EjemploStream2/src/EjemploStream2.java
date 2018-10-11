e170a9ab0cc5490f7ddb00d910744a03b8b4d3e0
adrilc24 <adrilc24@gmail.com>
2018-10-11 12:06:46 +0200

    Corregido EjemploStream2

--- EjemploStream2/src/EjemploStream2.java
+++ EjemploStream2/src/EjemploStream2.java
@@ -7,15 +7,26 @@ import java.util.Arrays;
 
 public class EjemploStream2 {
 	public static void main(String args[]) throws IOException {
-		ProcessBuilder pb = new ProcessBuilder ("ls -z");
-		//pb.directory (new File("/home/adrcre/PSP/HolaMundo1/bin/"));
-		Process process = new ProcessBuilder("ls -z").start() ;
+		ProcessBuilder pb = new ProcessBuilder ("ls", "-z");
+		
+		Process process = pb.start();
+		
 		InputStream is = process.getInputStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
+		
+		InputStream is2 = process.getErrorStream();
+		InputStreamReader isr2 = new InputStreamReader(is2);
+		BufferedReader br2 = new BufferedReader(isr2);
+		
 		String line;
 		System.out.println("Salida del proceso " + Arrays.toString(args) + " :");
 		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+		}
+		
+		System.out.println("Salida del proceso " + Arrays.toString(args) + " :");
+		while ((line = br2.readLine()) != null) {
 			System.out.println(line);
 		}
 	}
